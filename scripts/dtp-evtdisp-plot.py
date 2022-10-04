60e02b1cd3f660da07a6f22f49816132296d12cc
kwawrows <klaudia.wawrowska@gmail.com>
2022-10-04 20:04:08 +0200

    Fixing the display and channel numbering

--- scripts/dtp-evtdisp-plot.py
+++ scripts/dtp-evtdisp-plot.py
@@ -11,42 +11,62 @@ from pathlib import Path
 import click
 
 
-plt.rcParams['figure.figsize'] = [18., 10.]
 plt.rcParams['figure.dpi'] = 100
-plt.rcParams.update({'font.size': 12})
+plt.rcParams.update({'font.size': 14})
 plt.style.use("bmh") 
 
 def plotme_an_ED(df_adc, df_tp, run, ntsamples, zeroped, pdf = None):
 
     fir_delay = 16
+    ticks_per_sample = 32
     cmap = cm.get_cmap('bwr',2) #cmap for hit_continue param. 
     norm = colors.LogNorm() # cmap for raw adc data
+    # timestamp for the beginning of data capture that t will be plotted relative to
+    t0 = df_adc.index[0]
     
     #Prepare data for plotting 
-    chan = pd.to_numeric(df_adc.columns[1:]) 
     df_adc = df_adc.head(ntsamples) # only plot user-specified number of samples 
     Z = df_adc.to_numpy()[:,1:]
 
     #quick cheated pedsub
     if zeroped:
         Z = Z - np.mean(Z, axis = 0)
-        norm = colors.TwoSlopeNorm(vmin=np.min(Z), vcenter=0, vmax=np.max(Z)) #update cmap so it's centered at 0.
+        #update cmap so it's centered at 0.
+        norm = colors.TwoSlopeNorm(vmin=np.min(Z), vcenter=0, vmax=np.max(Z))
 
 
-    #2D plot of the raw ADC data
-    plt.imshow(Z.T, cmap = 'coolwarm',aspect = 'auto', origin = 'lower', norm = norm,
-               extent = [ min(df_adc.index),max(df_adc.index), min(chan), max(chan) ] )
-    # Overlay the FW hits 
-    plt.scatter(df_tp['peak_time'] - fir_delay + df_tp['ts'], df_tp['offline_ch'],  c = df_tp['hit_continue'],
-                s = 16, label = 'firmware hits', alpha  =0.9, cmap = cmap)
-    print( min(df_adc.index),max(df_adc.index), min(chan), max(chan) )
-    plt.ylabel('offline channel number')
-    plt.xlabel('timestamp [tick]')
-    plt.legend(title = "run number: %.0f" %run)
-    cb = plt.colorbar(ticks = [0.25,0.75], shrink =  0.7)
-    cb.ax.set_yticklabels(['0','1'])
-    cb.set_label("hit_continue", rotation = 270)
+    #convert the ylabels to account for channels not being consecutive     
+    y_vals = df_adc.columns.to_list()[0:]; y_vals = [int(i) for i in y_vals]
+
+    y=df_tp['offline_ch'].map(lambda k: y_vals.index(k))
+
+    yloc = [i for i in range(1,len(df_adc.columns),32)]
+    yticks = [str(df_adc.columns[i]) for i in yloc]
+
+
+
+    #Plot the data
+    fig, (ax1, ax2) = plt.subplots( 2, sharex= True, figsize = (15,10))
+
+    for i in [ax1, ax2]:
+        im = i.imshow(Z.T, cmap = 'coolwarm',aspect = 'auto', origin = 'lower', norm = norm)
+        i.set_ylabel('offline channel number')
+        i.set_xlim(0, len(Z))
+        i.set_yticks(yloc, yticks )
+        i.set_ylim(yloc[0],(yloc[-1]))
     
+    #Overlay the 2d hits
+    hits = ax2.scatter((df_tp['peak_time'] - fir_delay + (df_tp['ts'] -t0)/ticks_per_sample), y.values,
+                       c = df_tp['hit_continue'], s = 15, label = 'firmware hits', alpha  =0.6, cmap = cmap)
+    cb1 = plt.colorbar(im, ax = ax1, shrink = 0.7)
+    cb1.set_label("ADC ", rotation = 270, labelpad = +20)
+    cb2 = plt.colorbar(hits, ax = ax2, ticks = [0.25,0.75], shrink =  0.7)
+    cb2.ax.set_yticklabels(['0','1'])
+    cb2.set_label('hit_continue ', rotation = 270, labelpad = +20)
+    ax2.set_xlabel('relative time [tick]')
+    plt.legend(title = "run number: %.0f" %run)
+    plt.tight_layout()
+
     if pdf: pdf.savefig()
     plt.show()
     plt.close()
